e6d06ed4c669328ddda0d1eb155f74a4dbc7a6f6
Aditya Mali <adityamali2003@gmail.com>
2025-02-09 14:14:20 +0530

    fixed carousel + product Card implementation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,30 +4,43 @@ import {
   CarouselItem,
   CarouselNext,
   CarouselPrevious,
-} from "@/components/ui/carousel"
+} from "@/components/ui/carousel";
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardImage,
+  CardPrice,
+  CardTitle,
+} from "@/components/ui/card";
 import Image from "next/image";
 
 import { Cat } from "@/lib/samples/cat";
 
+import { products } from "@/data/productData";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center py-2 mt-24">
-
       {/* Carousel */}
-
-      <div className="text-center w-[calc(100vw-8rem)]">
-      <Carousel className="w-full px-8 h-96 rounded-lg">
-        <CarouselContent className="rounded">
-          <CarouselItem>
-            <Image src="/images/banner.jpeg" alt="1" width={400} height={100} className="object-cover h-96 w-full rounded-lg" />
-          </CarouselItem>
-          <CarouselItem>...</CarouselItem>
-          <CarouselItem>...</CarouselItem>
-        </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
-      </Carousel>
+      <div className="group w-[calc(100vw-8rem)] overflow-hidden">
+        <Carousel className="w-full">
+          <CarouselContent className="rounded">
+            <CarouselItem className="overflow-hidden">
+              <Image
+                src="/images/banner.jpeg"
+                alt="banner"
+                width={1920}
+                height={1080}
+                className="object-cover w-full h-[600px] rounded-lg"
+              />
+            </CarouselItem>
+            <CarouselItem>...</CarouselItem>
+            <CarouselItem>...</CarouselItem>
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
       </div>
 
       {/* For Baby / For Mom */}
@@ -37,9 +50,7 @@ export default function Home() {
           For Baby
           {/* <Image src="/images/baby.jpg" alt="1" width={100} height={100} className="object-cover h-12 w-12" /> */}
         </div>
-        <div className="bg-secondary rounded-lg p-4 text-center">
-          For Mom
-        </div>
+        <div className="bg-secondary rounded-lg p-4 text-center">For Mom</div>
       </div>
 
       {/* Product Categories */}
@@ -51,13 +62,39 @@ export default function Home() {
 
         <div className="flex items-center justify-center gap-10 p-6">
           {Cat.map((cat) => (
-            <div key={cat.id} className="bg-secondary rounded-full aspect-square h-32 w-32 p-6 text-center">
-              <Image src={cat.image} alt={cat.name} width={100} height={100} className="object-cover h-full w-full" />
+            <div
+              key={cat.id}
+              className="bg-secondary rounded-full aspect-square h-32 w-32 p-6 text-center"
+            >
+              <Image
+                src={cat.image}
+                alt={cat.name}
+                width={100}
+                height={100}
+                className="object-cover h-full w-full"
+              />
             </div>
           ))}
-        </div> 
+        </div>
       </div>
 
+      {/* Some Products */}
+      <div className="w-[calc(100vw-8rem)] mt-8">
+        <div className="flex flex-col justify-center mb-6">
+          <h5>Some Products</h5>
+        </div>
+        <div className="grid grid-cols-5 gap-4">
+          {products.slice(0, 5).map((product) => (
+            <Card key={product.id} className="w-full">
+              <CardImage />
+              <CardHeader>
+                <CardTitle className="text-base">{product.title}</CardTitle>
+                <CardPrice>Rs {product.price}/-</CardPrice>
+              </CardHeader>
+            </Card>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/ui/card.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import * as React from "react";
+import { cn } from "@/lib/utils";
+
+const Card = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn("rounded-lg text-card-foreground w-[250px]", className)}
+    {...props}
+  />
+));
+Card.displayName = "Card";
+
+const CardHeader = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn("flex flex-col space-y-1.5", className)}
+    {...props}
+  />
+));
+CardHeader.displayName = "CardHeader";
+
+const CardImage = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn("aspect-square bg-gray-200 rounded-lg", className)}
+    {...props}
+  />
+));
+CardImage.displayName = "CardImage";
+
+const CardTitle = React.forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLHeadingElement>
+>(({ className, ...props }, ref) => (
+  <h3
+    ref={ref}
+    className={cn(
+      "text-lg font-semibold leading-none tracking-tight",
+      className
+    )}
+    {...props}
+  />
+));
+CardTitle.displayName = "CardTitle";
+
+const CardDescription = React.forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLParagraphElement>
+>(({ className, ...props }, ref) => (
+  <p
+    ref={ref}
+    className={cn("text-sm text-muted-foreground", className)}
+    {...props}
+  />
+));
+CardDescription.displayName = "CardDescription";
+
+const CardPrice = React.forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLParagraphElement>
+>(({ className, ...props }, ref) => (
+  <p ref={ref} className={cn("text-lg font-semibold", className)} {...props} />
+));
+CardPrice.displayName = "CardPrice";
+
+export { Card, CardHeader, CardImage, CardTitle, CardDescription, CardPrice };
